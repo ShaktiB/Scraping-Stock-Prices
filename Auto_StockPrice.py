bb3ea721a79722d89202d8b297b65c22c5862ffc
Shaktisinh Bhati <shakti_1996@hotmail.com>
2020-06-17 20:42:33 -0400

    script now returns stock url

--- Auto_StockPrice.py
+++ Auto_StockPrice.py
@@ -43,9 +43,7 @@ def get_data(ticker, c, info):
             except:
                 pass
     
-    return [price, div_, freq]
-    
-    
+    return [price, div_, freq, url]  
 
 if __name__ == "__main__":
 
@@ -68,8 +66,9 @@ if __name__ == "__main__":
     stocks['Updated_Price'] = ''
     stocks['Div_Freq'] = ''
     stocks['Dividend'] = ''
+    stocks['Link'] = ''
     
-    stocks[['Updated_Price','Dividend','Div_Freq']] = stocks.apply(lambda x: get_data(x['Ticker'], x['Currency'], 'price'), axis=1, result_type='expand')
+    stocks[['Updated_Price','Dividend','Div_Freq','Link']] = stocks.apply(lambda x: get_data(x['Ticker'], x['Currency'], 'price'), axis=1, result_type='expand')
     
     stocks.set_index('Stock Name', inplace=True) # Need to assign names as index for use later when adding in scraped data
     
@@ -77,6 +76,7 @@ if __name__ == "__main__":
     cp_indx = list(df.columns).index('Current_Price') + 1 # Get column index of for 'Current Price'
     divFreq_indx = list(df.columns).index('Div_Frequency') + 1 # Get column index of for 'Current Price'
     div_indx = list(df.columns).index('Dividend') + 1 # Get column index of for 'Current Price'
+    link_indx = list(df.columns).index('Link') + 1 # Get column index of for 'Current Price'
     
     for rn in range(2,sheet.max_row+1):
         rowName = sheet.cell(row=rn, column = 1).value
@@ -84,6 +84,7 @@ if __name__ == "__main__":
             sheet.cell(row=rn,column=cp_indx).value = stocks.at[rowName,'Updated_Price'] # Requires stock names to be the row index
             sheet.cell(row=rn,column=divFreq_indx).value = stocks.at[rowName,'Div_Freq']
             sheet.cell(row=rn,column=div_indx).value = stocks.at[rowName,'Dividend']
+            sheet.cell(row=rn,column=link_indx).value = stocks.at[rowName,'Link']
     
     wb.save('Stock_Analysis.xlsx')
     
